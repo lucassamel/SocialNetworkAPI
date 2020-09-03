1a47f9e74b635ab02c7a42b6e6cb454f4f0ab0fa
Lucas Samel <lucas.samel97@gmail.com>
2020-09-03 18:07:49 -0300

    Lucas

---
+++ SocialNetworkBLL/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetworkBLL.Models
+{
+    class Post
+    {
+
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
--- SocialNetworkBLL/Models/Usuario.cs
+++ SocialNetworkBLL/Models/Usuario.cs
@@ -10,5 +10,6 @@ namespace SocialNetworkBLL.Models
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Email { get; set; }
+        public DateTime Aniversario { get; set; }
     }
 }
